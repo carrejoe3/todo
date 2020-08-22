7f7203ab305eeebc24c6f09d3678015f1017a69d
Joe <carre.joe3@gmail.com>
2020-08-22 14:30:12 +0100

    added a counter to the toolbar

--- client/src/components/Toolbar.vue
+++ client/src/components/Toolbar.vue
@@ -1,13 +1,20 @@
 <template>
   <v-toolbar color="primary" dark>
-    <v-toolbar-title>Todo</v-toolbar-title>
+    <v-toolbar-title>
+      Todos: {{ tasks.length }} items
+    </v-toolbar-title>
   </v-toolbar>
 </template>
 
 <script>
 
 export default {
-  name: 'Toolbar'
+  name: 'Toolbar',
+  computed: {
+    tasks () {
+      return this.$store.state.todos.tasks
+    }
+  }
 }
 
 </script>

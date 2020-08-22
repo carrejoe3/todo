7c45737b4fb5b35a99e1fe08e323f9c67c4b0355
Joe <carre.joe3@gmail.com>
2020-08-22 13:32:38 +0100

    created a todo input component

---
+++ client/src/components/TodoInput.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-container style="max-width: 500px" class="mt-15">
+    <v-text-field
+      v-model="task"
+      label="What are you working on?"
+      solo
+      @keydown.enter="createTodo"
+    >
+      <template v-slot:append>
+        <v-fade-transition>
+          <v-icon
+            v-if="task"
+            @click="createTodo"
+          >
+            mdi-plus-circle
+          </v-icon>
+        </v-fade-transition>
+      </template>
+    </v-text-field>
+  </v-container>
+</template>
+
+<script>
+
+export default {
+  name: 'TodoInput',
+  data: () => ({
+    task: null
+  }),
+  methods: {
+    createTodo () {
+    }
+  }
+}
+
+</script>
--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,17 +1,20 @@
 <template>
   <div class="home">
     <Toolbar />
+    <TodoInput />
   </div>
 </template>
 
 <script>
 
 import Toolbar from '@/components/Toolbar'
+import TodoInput from '@/components/TodoInput'
 
 export default {
   name: 'Home',
   components: {
-    Toolbar
+    Toolbar,
+    TodoInput
   }
 }
 
